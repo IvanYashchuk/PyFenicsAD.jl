1885d860aac9490449e1b30c149af38fdb663f52
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-03-21 18:08:14 +0200

    Updated fenics tests

--- test/test_zygote.jl
+++ test/test_zygote.jl
@@ -14,7 +14,8 @@ V = fenics.FunctionSpace(mesh, "P", 1)
 function assemble_fenics(u, kappa0, kappa1)
 
     f = fa.Expression(
-        "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2
+        "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)",
+        degree = 2,
     )
 
     inner, grad, dx = ufl.inner, ufl.grad, ufl.dx
@@ -28,14 +29,14 @@ end
 templates = (fa.Function(V), fa.Constant(0.0), fa.Constant(0.0))
 inputs = (ones(V.dim()), ones(1) * 0.5, ones(1) * 0.6)
 
-zygote_assemble_fenics(inputs...) = fem_eval(assemble_fenics, templates, inputs...)[1]
+zygote_assemble_fenics(inputs...) = evaluate_primal(assemble_fenics, templates, inputs...)[1]
 
 Zygote.@adjoint function zygote_assemble_fenics(inputs...)
-    pyout = pycall(fem_eval, PyObject, assemble_fenics, templates, inputs...)
-    numpy_output, fenics_output, fenics_inputs, tape = [get(pyout, PyObject, i) for i in 0:3]
+    pyout = pycall(evaluate_primal, PyObject, assemble_fenics, templates, inputs...)
+    numpy_output, fenics_output, fenics_inputs, tape = [get(pyout, PyObject, i) for i = 0:3]
 
     function vjp_fun(g)
-        vjp_out = vjp_fem_eval(g, fenics_output, fenics_inputs, tape)
+        vjp_out = evaluate_pullback(fenics_output, fenics_inputs, tape, g)
     end
 
     return get(pyout, 0), vjp_fun
@@ -66,9 +67,9 @@ end
     fdm_jac2 = FiniteDifferences.jacobian(fdm, hh1, inputs[2])
     fdm_jac3 = FiniteDifferences.jacobian(fdm, hh2, inputs[3])
 
-    @test isapprox(vjp_out[1], fdm_jac1[1]', atol=1e-5)
-    @test isapprox(vjp_out[2], fdm_jac2[1]', atol=1e-5)
-    @test isapprox(vjp_out[3], fdm_jac3[1]', atol=1e-5)
+    @test isapprox(vjp_out[1], fdm_jac1[1]', atol = 1e-5)
+    @test isapprox(vjp_out[2], fdm_jac2[1]', atol = 1e-5)
+    @test isapprox(vjp_out[3], fdm_jac3[1]', atol = 1e-5)
 end
 
 mesh = fa.UnitSquareMesh(6, 5)
@@ -77,7 +78,8 @@ V = fenics.FunctionSpace(mesh, "P", 1)
 function solve_fenics(kappa0, kappa1)
 
     f = fa.Expression(
-        "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2
+        "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)",
+        degree = 2,
     )
 
     u = fa.Function(V)
@@ -87,7 +89,7 @@ function solve_fenics(kappa0, kappa1)
     JJ = 0.5 * inner(kappa0 * grad(u), grad(u)) * dx - kappa1 * f * u * dx
     v = fenics.TestFunction(V)
     F = fenics.derivative(JJ, u, v)
-    fa.solve(F == 0, u, bcs=bcs)
+    fa.solve(F == 0, u, bcs = bcs)
     return u
 end
 
@@ -95,14 +97,14 @@ end
 templates = (fa.Constant(0.0), fa.Constant(0.0))
 inputs = (ones(1) * 0.5, ones(1) * 0.6)
 
-zygote_solve_fenics(inputs...) = fem_eval(solve_fenics, templates, inputs...)[1]
+zygote_solve_fenics(inputs...) = evaluate_primal(solve_fenics, templates, inputs...)[1]
 
 Zygote.@adjoint function zygote_solve_fenics(inputs...)
-    pyout = pycall(fem_eval, PyObject, solve_fenics, templates, inputs...)
-    numpy_output, fenics_output, fenics_inputs, tape = [get(pyout, PyObject, i) for i in 0:3]
+    pyout = pycall(evaluate_primal, PyObject, solve_fenics, templates, inputs...)
+    numpy_output, fenics_output, fenics_inputs, tape = [get(pyout, PyObject, i) for i = 0:3]
 
     function vjp_fun(g)
-        vjp_out = vjp_fem_eval(g, fenics_output, fenics_inputs, tape)
+        vjp_out = evaluate_pullback(fenics_output, fenics_inputs, tape, g)
     end
 
     return get(pyout, 0), vjp_fun
